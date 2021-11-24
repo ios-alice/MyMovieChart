136a1b7d2d7273075eeecb794a6989a8e738751f
sigma0to9 <sigma0to9@naver.com>
2021-11-24 15:26:49 +0900

    Make custom tableviewcell

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -10,8 +10,11 @@ class ListViewController : UITableViewController {
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
+    // lazy : 클래스의 인스턴스 생성 시 초기화 아닌, 프로퍼티를 호출 해야만 초기화.
+    // 클로저 구문을 사용한 프로퍼티 초기화 : 연산으로 얻어진 값을 이용하여 초기화
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
+        //for - in 구문 : in을 반복하여 for에 대입한다.
         for (title, desc, opendate, rating) in self.dataset {
             let mvo = MovieVO()
             mvo.title = title
@@ -22,6 +25,7 @@ class ListViewController : UITableViewController {
             datalist.append(mvo)
         }
         return datalist
+        // 변수의 초기값은 datalist
     }()
     
 
@@ -30,6 +34,33 @@ class ListViewController : UITableViewController {
 
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //행에 맞는 데이터소스 읽어옴
+        let row = self.list[indexPath.row]
+        
+        // 재사용 큐를 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        
+        let title = cell.viewWithTag(101) as? UILabel
+        let desc = cell.viewWithTag(102) as? UILabel
+        let opendate = cell.viewWithTag(103) as? UILabel
+        let rating = cell.viewWithTag(104) as? UILabel
+        
+        title?.text = row.title
+        desc?.text = row.description
+        opendate?.text = row.opendate
+        rating?.text = "\(row.rating!)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은\(indexPath.row)번째 행입니다.")
+    }
     
 }
