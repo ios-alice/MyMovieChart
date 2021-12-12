068a560e148f56b078aa18758bd0fbb4c4c8a855
ios-alice <apple12347878@gmail.com>
2021-12-13 08:02:00 +0900

    영화관 정보 api

---
+++ MyMovieChart/TheaterCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class TheaterCell : UITableViewCell {
+    // 상영관명
+    @IBOutlet weak var name: UILabel!
+    
+    //연락처
+    @IBOutlet weak var tel: UILabel!
+    
+    //주소
+    @IBOutlet weak var addr: UILabel!
+}
