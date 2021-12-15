548da2679ccd19984c89966aebdae0a520c97b2a
ios-alice <apple12347878@gmail.com>
2021-12-16 00:30:21 +0900

    MapKit FW

--- MyMovieChart/TheaterViewController.swift
+++ MyMovieChart/TheaterViewController.swift
@@ -1,82 +1,32 @@
 import UIKit
+import MapKit
 
-
-class TheaterListController : UITableViewController {
+class TheaterViewController : UIViewController {
+    //전달되는 데이터를 받을 변수
+    var param: NSDictionary!
     
-    // API를 통해서 온 데이터를 저장할 배열 변수
-    // 테이블 뷰에 넣을 배열형태의 데이터 집합
-    var list = [NSDictionary]()
+    @IBOutlet weak var map: MKMapView!
     
-    // 읽어올 데이터의 시작 위치
-    var startPoint = 0
-    
-    // 1. 뷰컨트롤러가 메모리에 로드 되고 call 되는 함수를 오버로딩
     override func viewDidLoad() {
+        self.navigationItem.title = self.param["상영관명"] as? String
         
-        //self:나 또는 부모의 인스턴스의
-        // 2. callTeaterAPI()호출
-        self.callTheaterAPI()
+        // 위도와 경도를 추출, Double 값으로 캐스팅
+        // 위도와 경도는 String으로 받아지는데 NSString의 .doubleVlaue 속성으로, 필요한 Double 타입으로 변환한다.
+        let lat = (param?["위도"] as! NSString).doubleValue
+        let lng = (param?["경도"] as! NSString).doubleValue
+        
+        // 위도와 경도를 인수로하는 2D위치정보 객체 정의
+        // 중심좌표를 정한다.
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+        
+        // 지도에 표현 될 거리 (단위는 m)
+        let regionRadius : CLLocationDistance = 100
+        
+        // 지도 데이터 생성 : 거리를 반영한 지역정보를 조합한
+        let coordinateRegin = MKCoordinateRegion(center: location, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
+        // 지도 객체에 (map) 데이터 전달하여 화면 표시
+        self.map.setRegion(coordinateRegin, animated: true)
     }
     
-    // 3.API로부터 극장 정보를 읽어오는 메소드
-    func callTheaterAPI() {
-
-        let requestURI = "http://swiftapi.rubypaper.co.kr:2029/theater/list" //API 요청주소
-        let sList = 100 // 불러올 데이터 개수
-        let type = "json" // 데이터 형식
-        
-        // 4.인자값을 모아 URL 객체로 정의한다.. = String => URL 타입 (API호출은 URL 타입으로 함)
-        let urlObj = URL(string: "\(requestURI)?s_page=\(self.startPoint)&s_list=\(sList)&type=\(type)")
-        
-        do {
-            // Point1. Rest API 호출 : NSString(contentsOf:encoding:) : 객체를 이용하여 API를호출하고 그 결과값을 인코딩을 지정한 문자열(EUC-KR=080000522)로 받아온다.
-            // 영화관목록은 한글까지 문제없이 처리하는 UTF-8과는 달리, EUC-KR로 되어있어서 데이터를 읽어올 때 EUC-KR로 지정해서? 인코딩하면서 호출
-            let stringdata = try NSString(contentsOf: urlObj!, encoding: 0x80_000_422)
-            
-            // Point2. Data 객체 생성 ; 문자열로 받은 데이터를 -> UTF-8로 인코딩처리한 Data타입으로 변환한다. (EUC-KR -> UTF-8)
-            // Data 타입은 utf-8 밖에 못받나봄?
-            let encdata = stringdata.data(using:String.Encoding.utf8.rawValue)
-            
-            do {
-                // Point3. 파싱 : Data 객체를 파싱하여 NSArray 객체로 변환한다. Json 배열타입임으로 타입캐스팅한다.
-                let apiArray = try JSONSerialization.jsonObject(with: encdata!, options: []) as? NSArray
-                
-                // 읽어온 데이터르르 순회하며 self.list  배열에 추가한다.
-                for obj in apiArray! {
-                    self.list.append(obj as! NSDictionary)
-                }
-        } catch {
-            //경고창 형식으로 오류 메시지를 표시해준다.
-            let alert = UIAlertController(title: "실패", message: "데이터 분석에 실패하였습니다.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title:"확인", style: .cancel))
-            self.present(alert, animated: false)
-        }
-        // 9.읽어와야 할 다음 페이지의 데이터 시작 위치를 구해 저장해둔다.
-            self.startPoint += sList
-    } catch {
-    //경고창 형식으로 오류를 표시해준다.
-        let alert = UIAlertController(title: "실패", message: "데이터를 불러오는데 실패했습니다", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
-        self.present(alert, animated: false)
-    }
-    
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.list.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //self.list 배열에서 행에 맞는 데이터 꺼냄
-        let obj = self.list[indexPath.row]
-        
-        //재사용 큐로부터 tCell 식별자에 맞는 셀 객체를 전달받음
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tCell") as! TheaterCell
-        
-        cell.name?.text = obj["상영관명"] as? String
-        cell.tel?.text = obj["연락처"] as? String
-        cell.addr?.text = obj["소재지도로명주소"] as? String
-        
-        return cell
-    }
 }
